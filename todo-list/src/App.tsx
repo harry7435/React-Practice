aa2c3b8e1d00dcec73611834a1ef11aa503ad008
ikjun <harry960629@naver.com>
2023-05-04 17:29:11 +0900

    Feat: 추가중 TodoInput 입력창 state

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,15 +1,17 @@
-import "./App.css";
-import Divider from "./Divder/Divider";
-import TodoHeader from "./Header/TodoHeader";
-import TodoInput from "./Input/TodoInput";
-import TodoList from "./List/TodoList";
-import TodoListTools from "./Tools/TodoListTools";
+import { useState } from 'react';
+import './App.css';
+import Divider from './Divder/Divider';
+import TodoHeader from './Header/TodoHeader';
+import TodoInput from './Input/TodoInput';
+import TodoList from './List/TodoList';
+import TodoListTools from './Tools/TodoListTools';
 
 function App() {
+  const [text, setText] = useState('');
   return (
     <main className="App">
       <TodoHeader />
-      <TodoInput />
+      <TodoInput text={text} />
       <TodoListTools />
       <Divider />
       <TodoList />
--- todo-list/src/Input/TodoInput.tsx
+++ todo-list/src/Input/TodoInput.tsx
@@ -1,11 +1,20 @@
+import { ChangeEvent } from 'react';
 import { RiChatNewLine } from 'react-icons/ri';
 import styles from './TodoInput.module.css';
 
-const TodoInput = () => {
+interface TodoInputProps {
+  text: string;
+}
+
+const TodoInput = (props: TodoInputProps) => {
+  const handleInputChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+  };
+
   return (
     <section className={styles.container}>
       <form className={styles.formContainer}>
-        <input className={styles.input} placeholder={'해야할 Todo'} />
+        <input className={styles.input} placeholder={'해야할 Todo'} value={props.text} onChange={handleInputChanged} />
       </form>
       <button className={styles.enter}>
         <RiChatNewLine />
