c6d5a5f902b4718033edc71b9c48ff3870b43293
Cho-Ik-Jun <harry960629@naver.com>
2023-05-05 18:04:34 +0900

    Feat: 추가 할일 입력 submit 기능

--- todo-list/src/Input/TodoInput.tsx
+++ todo-list/src/Input/TodoInput.tsx
@@ -1,24 +1,36 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, FormEvent } from 'react';
 import { RiChatNewLine } from 'react-icons/ri';
 import styles from './TodoInput.module.css';
 
 interface TodoInputProps {
   text: string;
+  onTextChange: (text: string) => void;
+  onSubmit: () => void;
 }
 
 const TodoInput = (props: TodoInputProps) => {
   const handleInputChanged = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value);
+    props.onTextChange(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    props.onSubmit();
   };
 
   return (
     <section className={styles.container}>
-      <form className={styles.formContainer}>
-        <input className={styles.input} placeholder={'해야할 Todo'} value={props.text} onChange={handleInputChanged} />
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
+        <input
+          className={styles.input}
+          placeholder={'해야할 Todo'}
+          value={props.text}
+          onChange={handleInputChanged}
+        />
+        <button type="submit" className={styles.enter}>
+          <RiChatNewLine />
+        </button>
       </form>
-      <button className={styles.enter}>
-        <RiChatNewLine />
-      </button>
     </section>
   );
 };
