8352fbe8a2cd44db3a505552a9360c1565f4d0b9
Cho-Ik-Jun <harry960629@naver.com>
2023-05-06 19:40:42 +0900

    Feat: 추가 포켓몬도감 프로젝트, 초기 레이아웃 설정

---
+++ poke-dex/src/App.tsx
@@ -0,0 +1,13 @@
+import PageHeader from './Common/PageHeader';
+import PokeCardList from './List/PokeCardList';
+
+function App() {
+  return (
+    <>
+      <PageHeader />
+      <PokeCardList />
+    </>
+  );
+}
+
+export default App;
