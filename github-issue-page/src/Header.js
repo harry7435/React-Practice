99e148963f161c1b8b81d17f99a2730ca234aebe
ikjun <harry960629@naver.com>
2023-05-10 17:27:18 +0900

    Design: 헤더 레이아웃 변경

--- github-issue-page/src/Header.js
+++ github-issue-page/src/Header.js
@@ -1,39 +1,43 @@
 import styles from './Header.module.css';
 import Button from './components/Button';
 import Space from './components/Space';
+import Tab from './components/Tab';
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <Button
-        style={{
-          fontSize: '14px',
-          backgroundColor: 'transparent',
-          color: 'black',
-        }}
-      >
-        Watch
-      </Button>
-      <Space />
-      <Button
-        style={{
-          fontSize: '14px',
-          backgroundColor: 'transparent',
-          color: 'black',
-        }}
-      >
-        Fork <div className={styles.circle}>5</div>
-      </Button>
-      <Space />
-      <Button
-        style={{
-          fontSize: '14px',
-          backgroundColor: 'transparent',
-          color: 'black',
-        }}
-      >
-        Star
-      </Button>
+      <div className={styles.buttonContainer}>
+        <Button
+          style={{
+            fontSize: '14px',
+            backgroundColor: 'transparent',
+            color: 'black',
+          }}
+        >
+          Watch
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: '14px',
+            backgroundColor: 'transparent',
+            color: 'black',
+          }}
+        >
+          Fork <div className={styles.circle}>5</div>
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: '14px',
+            backgroundColor: 'transparent',
+            color: 'black',
+          }}
+        >
+          Star
+        </Button>
+      </div>
+      <Tab />
     </div>
   );
 }
