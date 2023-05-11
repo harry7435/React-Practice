d4fe474ab79f425960132f49836c9032da44ffb1
ikjun <harry960629@naver.com>
2023-05-11 15:36:45 +0900

    Refactor: 뱃지 컴포넌트 리스트화

--- github-issue-page/src/ListContainer.js
+++ github-issue-page/src/ListContainer.js
@@ -32,7 +32,14 @@ export default function ListContainer() {
         </div>
       </ListItemLayout>
       <div className={styles.container}>
-        <ListItem />
+        <ListItem
+          badges={[
+            {
+              color: 'red',
+              title: 'Bug',
+            },
+          ]}
+        />
       </div>
     </div>
   );
--- github-issue-page/src/components/ListItem.js
+++ github-issue-page/src/components/ListItem.js
@@ -2,13 +2,21 @@ import Badge from './Badge';
 import styles from './ListItem.module.css';
 import ListItemLayout from './ListItemLayout';
 
-export default function ListItem({ checked, onChangeCheckBox, onClickTitle }) {
+export default function ListItem({
+  checked,
+  onChangeCheckBox,
+  onClickTitle,
+  badges,
+}) {
   return (
     <ListItemLayout>
       <div>
         <div role="button" onClick={onClickTitle} className={styles.title}>
           Issue Example
-          <Badge title="Bug" color="red" />
+          {badges &&
+            badges.map((badgeProps, idx) => (
+              <Badge key={idx} {...badgeProps} />
+            ))}
         </div>
         <div className={styles.description}># Description</div>
       </div>
