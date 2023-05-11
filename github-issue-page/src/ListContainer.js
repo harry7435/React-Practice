9ae19abbf4425f533b828d02c4a439c2aeab3fde
ikjun <harry960629@naver.com>
2023-05-11 16:17:02 +0900

    Feat: OpenCloseFilter 추가
    - open, close 상태 필터 추가
    - usestate로 클릭 상태 관리

--- github-issue-page/src/ListContainer.js
+++ github-issue-page/src/ListContainer.js
@@ -3,6 +3,7 @@ import Button from './components/Button';
 import styles from './ListContainer.module.css';
 import ListItem from './components/ListItem';
 import ListItemLayout from './components/ListItemLayout';
+import cx from 'clsx';
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState('is:pr is:open');
@@ -21,6 +22,7 @@ export default function ListContainer() {
           New Issue
         </Button>
       </div>
+      <OpenClosedFilters />
       <ListItemLayout className={styles.listFilter}>
         <div className={styles.filterLists}>
           <span>Author</span>
@@ -42,5 +44,42 @@ export default function ListContainer() {
         />
       </div>
     </div>
+  );
+}
+
+function OpenClosedFilters({ data }) {
+  const [isOpenMode, setIsOpenMode] = useState(true);
+
+  const openModeDataSize = 1;
+  const closeModeDataSize = 2;
+
+  return (
+    <>
+      <OpenClosedFilter
+        size={openModeDataSize}
+        state="Open"
+        selected={isOpenMode}
+        onClick={() => setIsOpenMode(true)}
+      />
+      <OpenClosedFilter
+        size={closeModeDataSize}
+        state="Closed"
+        selected={!isOpenMode}
+        onClick={() => setIsOpenMode(false)}
+      />
+    </>
+  );
+}
+
+function OpenClosedFilter({ size, state, onClick, selected }) {
+  return (
+    <span
+      role="button"
+      className={cx(styles.textFilter, { [styles.selected]: selected })}
+      onClick={onClick}
+    >
+      {size}
+      {state}
+    </span>
   );
 }
