ce0e9944130c7a1f4249691cabf8474ab45e0445
Cho-Ik-Jun <harry960629@naver.com>
2023-07-12 22:53:25 +0900

    Feat: 리스트 필터 기능 추가

--- github-issue-page/src/ListContainer.js
+++ github-issue-page/src/ListContainer.js
@@ -7,16 +7,16 @@ import ListItemLayout from './components/ListItemLayout';
 import Pagination from './components/Pagination';
 import ListFilter from './components/ListFilter';
 import OpenClosedFilters from './components/OpenClosedFilter';
+import { GITHUB_API } from './api';
 
 import styles from './ListContainer.module.css';
-
-const GITHUB_API = 'https://api.github.com';
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState('is:pr is:open');
   const [list, setList] = useState([]);
   const [page, setPage] = useState(1);
   const [isOpenMode, setIsOpenMode] = useState(true);
+  const [params, setParams] = useState();
   const maxPage = 10;
 
   async function getData(params) {
@@ -30,10 +30,8 @@ export default function ListContainer() {
   }
 
   useEffect(() => {
-    getData({ page, state: isOpenMode ? 'open' : 'closed' });
-  }, [page, isOpenMode]);
-
-  console.log({ list });
+    getData({ page, state: isOpenMode ? 'open' : 'closed', ...params });
+  }, [page, isOpenMode, params]);
 
   return (
     <>
@@ -61,8 +59,9 @@ export default function ListContainer() {
         <div className={styles.container}>
           <ListItemLayout className={styles.listFilter}>
             <ListFilter
-              onChangeFilter={(filteredData) => {
+              onChangeFilter={(params) => {
                 // 필터링된 요소에 맞게 데이터를 불러오기
+                setParams(params);
               }}
             />
           </ListItemLayout>
--- github-issue-page/src/components/Modal.js
+++ github-issue-page/src/components/Modal.js
@@ -13,14 +13,27 @@ export default function Modal({
   const [searchValue, setSearchValue] = useState('');
   const [filteredData, setFilteredData] = useState(searchDataList);
 
+  // 검색어 로우 데이터 보존
   useEffect(() => {
-    setFilteredData(searchDataList.filter((item) => item === searchValue));
+    setFilteredData(searchDataList);
+  }, [searchDataList]);
+
+  // 검색 필터링
+  useEffect(() => {
+    if (searchValue === '') {
+      setFilteredData(searchDataList);
+    } else {
+      const filteredSearchList = searchDataList.filter((item) =>
+        item.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFilteredData(filteredSearchList);
+    }
   }, [searchDataList, searchValue]);
 
   return (
     <div className={cx(styles.modal, { [styles.opened]: opened })}>
       <div className={styles.header}>
-        <span>{title}</span>
+        <span>Filter by {title}</span>
         <button onClick={onClose}>X</button>
       </div>
       <div className={styles.input}>
@@ -30,11 +43,23 @@ export default function Modal({
           onChange={(e) => setSearchValue(e.target.value)}
         />
       </div>
-      {filteredData.map((data) => (
-        <div key={data} onClick={onClickCell} role="button">
-          {data}
-        </div>
-      ))}
+      <div className={styles.list}>
+        {filteredData.map((data) => (
+          <div
+            key={data.name}
+            onClick={() => {
+              const isLabel = title.toLowerCase() === 'label';
+              const paramKey = isLabel ? 'labels' : title.toLowerCase();
+
+              onClickCell({ [paramKey]: data.name });
+            }}
+            className={styles.item}
+            role="button"
+          >
+            {data.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
